19608b14fedaae14927baa01ac1c43e82949ade5
Bogdan <noskovbogdan@gmail.com>
2021-12-04 18:30:19 +0300

    feat: add production webpack config

---
+++ webpack.config.production.js
@@ -0,0 +1,50 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  // entry: './src/index.ts',
+  // output: {
+  //   path: path.resolve(__dirname, 'dist'),
+  //   filename: 'bundle.js',
+  // },
+
+  mode: "production",
+
+  devtool: "none",
+
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(s[ac]|c)ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: ["babel-loader", "ts-loader"],
+      },
+    ],
+  },
+
+  plugins: [new MiniCssExtractPlugin()],
+
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+};
